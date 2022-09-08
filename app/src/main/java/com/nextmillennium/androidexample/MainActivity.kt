251ebc420e64c90864d361b18b19f71b3ce17156
Artem Nazarov <art_nazarov@mail.ru>
2022-09-08 14:11:27 +0300

    Updated dependencies. Added sdk version param. Updated gitignore.

--- app/src/main/java/com/nextmillennium/androidexample/MainActivity.kt
+++ app/src/main/java/com/nextmillennium/androidexample/MainActivity.kt
@@ -3,14 +3,14 @@ package com.nextmillennium.androidexample
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
 import com.nextmillennium.androidexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
