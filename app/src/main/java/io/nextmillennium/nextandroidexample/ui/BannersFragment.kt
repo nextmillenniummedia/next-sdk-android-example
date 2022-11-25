5f98de9015ca5b21dbc884b2149f63dfa8aecd5b
Artem Nazarov <art_nazarov@mail.ru>
2022-11-25 19:08:46 +0300

    Updated examples for banners and native ads

--- app/src/main/java/io/nextmillennium/nextandroidexample/ui/BannersFragment.kt
+++ app/src/main/java/io/nextmillennium/nextandroidexample/ui/BannersFragment.kt
@@ -1,6 +1,7 @@
 package io.nextmillennium.nextandroidexample.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,94 +11,136 @@ import androidx.preference.PreferenceManager
 import com.google.android.material.snackbar.Snackbar
 import io.nextmillennium.nextandroidexample.R
 import io.nextmillennium.nextandroidexample.databinding.FragmentBannersBinding
+import io.nextmillennium.nextsdk.network.NextAdError
 import io.nextmillennium.nextsdk.network.callbacks.FetchListener
+import io.nextmillennium.nextsdk.ui.BaseAdContainer
+import io.nextmillennium.nextsdk.ui.NextAdListener
 import io.nextmillennium.nextsdk.ui.banner.NextBannerView
 
 class BannersFragment : Fragment() {
 
-    private lateinit var binding: FragmentBannersBinding
-    private lateinit var simpleBanner: NextBannerView
-    private lateinit var mediumRectangle: NextBannerView
-    private lateinit var anchoredBanner: NextBannerView
-    private lateinit var inlineBanner: NextBannerView
+    private var binding: FragmentBannersBinding? = null
+    private var simpleBanner: NextBannerView? = null
+    private var mediumRectangle: NextBannerView? = null
+    private var anchoredBanner: NextBannerView? = null
+    private var inlineBanner: NextBannerView? = null
     private val showSnackbar = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = FragmentBannersBinding.inflate(inflater)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        simpleBanner = binding.bannerSimple
-        mediumRectangle = binding.bannerMrec
-        anchoredBanner = binding.bannerAnchored
-        inlineBanner = binding.bannerInline
-        simpleBanner.unitId = preferences.getString("banner", "103")
-        mediumRectangle.unitId = preferences.getString("banner_mrec", "104")
-        anchoredBanner.unitId = preferences.getString("banner_anchored", "105")
-        inlineBanner.unitId = preferences.getString("banner_inline", "112")
-        val load = binding.loadBanners
-        mediumRectangle.isCollapsible = true
-        inlineBanner.setContainerWidth(binding.inlineBannerContainer.width)
-        load.setOnClickListener {
-            simpleBanner.setFetchListener(createListener(simpleBanner.unitId))
-            simpleBanner.load()
-            anchoredBanner.setFetchListener(createListener(anchoredBanner.unitId))
-            anchoredBanner.load()
-            mediumRectangle.setFetchListener(createListener(mediumRectangle.unitId))
-            mediumRectangle.load()
-            inlineBanner.setFetchListener(createListener(inlineBanner.unitId))
-            inlineBanner.load()
+        simpleBanner = binding?.bannerSimple
+        mediumRectangle = binding?.bannerMrec
+        anchoredBanner = binding?.bannerAnchored
+        inlineBanner = binding?.bannerInline
+        simpleBanner?.setAdListener(createAdListener(simpleBanner))
+        mediumRectangle?.setAdListener(createAdListener(mediumRectangle))
+        inlineBanner?.setAdListener(createAdListener(inlineBanner))
+        simpleBanner?.unitId = preferences.getString("banner", "103")
+        mediumRectangle?.unitId = preferences.getString("banner_mrec", "104")
+        anchoredBanner?.unitId = preferences.getString("banner_anchored", "105")
+        inlineBanner?.unitId = preferences.getString("banner_inline", "112")
+        mediumRectangle?.isCollapsible = true
+        binding?.let {
+            inlineBanner?.setContainerWidth(it.inlineBannerContainer.width)
+        }
+        binding?.loadBanners?.setOnClickListener {
+            simpleBanner?.setFetchListener(createListener(simpleBanner?.unitId ?: ""))
+            simpleBanner?.load()
+            anchoredBanner?.setFetchListener(createListener(anchoredBanner?.unitId ?: ""))
+            anchoredBanner?.load()
+            mediumRectangle?.setFetchListener(createListener(mediumRectangle?.unitId ?: ""))
+            mediumRectangle?.load()
+            inlineBanner?.setFetchListener(createListener(inlineBanner?.unitId ?: ""))
+            inlineBanner?.load()
         }
     }
+
+    private fun createAdListener(ad: NextBannerView?): NextAdListener {
+        return object : NextAdListener {
+            override fun onAdLoaded(container: BaseAdContainer?) {
+                Log.d("NEXT_SDK", "Successfully loaded ad ${ad?.id}")
+            }
+
+            override fun onAdClicked() {
+                Log.d("NEXT_SDK", "Successfully tracked click for ${ad?.id}")
+            }
+
+            override fun onAdImpression() {
+                Log.d("NEXT_SDK", "Successfully tracked impression for ${ad?.id}")
+            }
+
+            override fun onAdClosed() {
+                Log.d("NEXT_SDK", "Closed ad ${ad?.id}")
+            }
+
+            override fun onAdLoadFail(adError: NextAdError?) {
+                Log.e("NEXT_SDK", adError.toString())
+            }
+
+            override fun onAdOpened() {
+                Log.d("NEXT_SDK", "Opened ad ${ad?.id}")
+            }
+        }
+    }
+
 
     private fun createListener(unitId: String): FetchListener {
         return object : FetchListener {
             override fun onSuccess() {
                 if (!showSnackbar) return
-                Snackbar.make(
-                    binding.root,
-                    "Successfully loaded banner : $unitId",
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                binding?.let {
+                    Snackbar.make(
+                        it.root,
+                        "Successfully loaded banner : $unitId",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
             }
 
             override fun onError(err: Throwable?) {
                 if (!showSnackbar) return
-                Snackbar.make(binding.root, "Error banner load: $err", Snackbar.LENGTH_SHORT)
-                    .setBackgroundTint(
-                        ContextCompat.getColor(
-                            requireContext(),
-                            R.color.deep_orange_A700
-                        )
-                    ).show()
+                binding?.let {
+                    Snackbar.make(it.root, "Error banner load: $err", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.deep_orange_A700
+                            )
+                        ).show()
+                }
             }
         }
     }
 
+
     override fun onResume() {
         super.onResume()
-        simpleBanner.resume()
-        anchoredBanner.resume()
-        mediumRectangle.resume()
+        simpleBanner?.resume()
+        anchoredBanner?.resume()
+        mediumRectangle?.resume()
     }
 
     override fun onPause() {
-        simpleBanner.pause()
-        anchoredBanner.pause()
-        mediumRectangle.pause()
+        simpleBanner?.pause()
+        anchoredBanner?.pause()
+        mediumRectangle?.pause()
         super.onPause()
     }
 
     override fun onDestroyView() {
-        simpleBanner.destroy()
-        anchoredBanner.destroy()
-        mediumRectangle.destroy()
+        simpleBanner?.destroy()
+        anchoredBanner?.destroy()
+        mediumRectangle?.destroy()
+        binding = null
         super.onDestroyView()
     }
 }
--- app/src/main/java/io/nextmillennium/nextandroidexample/ui/NativeFragment.kt
+++ app/src/main/java/io/nextmillennium/nextandroidexample/ui/NativeFragment.kt
@@ -1,68 +1,102 @@
 package io.nextmillennium.nextandroidexample.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RadioGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import com.google.android.material.snackbar.Snackbar
 import io.nextmillennium.nextandroidexample.R
 import io.nextmillennium.nextandroidexample.databinding.FragmentNativeBinding
+import io.nextmillennium.nextsdk.network.NextAdError
 import io.nextmillennium.nextsdk.network.callbacks.FetchListener
+import io.nextmillennium.nextsdk.ui.BaseAdContainer
+import io.nextmillennium.nextsdk.ui.NextAdListener
+import io.nextmillennium.nextsdk.ui.nativeads.NextNativeView
 
-class NativeFragment : Fragment() {
+class NativeFragment : Fragment(), NextAdListener {
 
-    private lateinit var binding: FragmentNativeBinding
+    private var binding: FragmentNativeBinding? = null
+    private var nativeView: NextNativeView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = FragmentNativeBinding.inflate(inflater)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        val native = binding.nativeView
-        val nativeType: RadioGroup = binding.nativeTypeGroup
-        val load: Button = binding.loadNative
+        nativeView = binding?.nativeView
+        nativeView?.setAdListener(this)
+        val nativeType = binding?.nativeTypeGroup
+        val load = binding?.loadNative
         val smallId = preferences.getString("native_small", "108")
         val mediumId = preferences.getString("native_medium", "109")
-        load.setOnClickListener {
+        load?.setOnClickListener {
+            nativeView?.destroy()
             val unitId =
-                if (nativeType.checkedRadioButtonId == R.id.radio_native_small) smallId
+                if (nativeType?.checkedRadioButtonId == R.id.radio_native_small) smallId
                 else mediumId
-            native.unitId = unitId
-            native.setFetchListener(object : FetchListener {
+            nativeView?.unitId = unitId
+            nativeView?.setFetchListener(object : FetchListener {
                 override fun onSuccess() {
-                    Snackbar.make(
-                        binding.root,
-                        "Successfully loaded banner : ${native.unitId}",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
+                    binding?.let {
+                        Snackbar.make(
+                            it.root,
+                            "Successfully loaded banner : ${nativeView?.unitId}",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
                 }
 
                 override fun onError(err: Throwable?) {
-                    Snackbar.make(binding.root, "Error banner load: $err", Snackbar.LENGTH_SHORT)
-                        .setBackgroundTint(
-                            ContextCompat.getColor(
-                                requireContext(),
-                                R.color.deep_orange_A700
+                    binding?.let {
+                        Snackbar.make(it.root, "Error banner load: $err", Snackbar.LENGTH_SHORT)
+                            .setBackgroundTint(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.deep_orange_A700
+                                )
                             )
-                        )
-                        .show()
+                            .show()
+                    }
                 }
             })
-            if (nativeType.checkedRadioButtonId == R.id.radio_native_custom) {
-                native.setResourceId(R.layout.custom_native)
+            if (nativeType?.checkedRadioButtonId == R.id.radio_native_custom) {
+                nativeView?.setResourceId(R.layout.custom_native)
             }
-            native.load()
+            nativeView?.load()
         }
+    }
+
+    override fun onAdLoaded(container: BaseAdContainer?) {
+        nativeView = container as NextNativeView
+        Log.d("NEXT_SDK", "Successfully loaded ad : ${container.id}")
+    }
+
+    override fun onAdClicked() {
+        Log.d("NEXT_SDK", "Successfully tracked click")
+    }
+
+    override fun onAdImpression() {
+        Log.d("NEXT_SDK", "Successfully tracked impression")
+    }
+
+    override fun onAdLoadFail(adError: NextAdError?) {
+        Log.e("NEXT_SDK", adError.toString())
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        nativeView?.destroy()
+        binding = null
     }
 }
